2c874cc244ccc3cc6350834d8dec7fb604eafb16
koushiiik <koushikmahanta52@gmail.com>
2025-12-08 00:04:34 +0530

    push element at bottom of stack

---
+++ push_at_bottom_of_stack.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void pushAtBottom(stack<int>&s,int val)
+{
+    if(s.empty()){
+        s.push(val);
+        return;
+    }
+    int temp=s.top();
+    s.pop();
+    pushAtBottom(s,val);
+    s.push(temp);
+}
+int main()
+{
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    pushAtBottom(s,5);
+
+    while(!s.empty())
+    {
+        cout<<s.top()<<" ";
+        s.pop();
+
+    }
+    cout<<endl;
+    return 0;
+}
+
